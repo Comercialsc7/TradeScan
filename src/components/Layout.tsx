1559ace4726899779a59cb6ec6a05806981d8246
Skip <skip@usecurling.com>
2025-10-28 20:51:10 +0000

    feat: Add theme toggle switch to index component

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,11 +1,17 @@
 import { Outlet } from 'react-router-dom'
+import { ThemeToggle } from '@/components/theme-toggle'
 
 export default function Layout() {
   return (
-    <main className="flex min-h-screen w-full items-center justify-center p-4">
-      <div className="w-full max-w-md">
-        <Outlet />
+    <>
+      <div className="absolute right-4 top-4 md:right-8 md:top-8">
+        <ThemeToggle />
       </div>
-    </main>
+      <main className="flex min-h-screen w-full items-center justify-center p-4">
+        <div className="w-full max-w-md">
+          <Outlet />
+        </div>
+      </main>
+    </>
   )
 }
